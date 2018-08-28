051a02accfbb47746c448029552611858c0835f0
Yurina Shin <yurinas@gmail.com>
2018-08-28 17:09:17 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all =[]
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
 end
