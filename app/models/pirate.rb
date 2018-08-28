cb2239d288e0b1e3e8449481a1ae066bce12cd4b
Yurina Shin <yurinas@gmail.com>
2018-08-28 17:07:23 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,3 +1,14 @@
 class Pirate
   attr_accessor :name, :weight, :height
+  @@all = []
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
 end
